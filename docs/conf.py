2fe56443936b7cd4af3afc1fd3cae8960a6acf01
prof79 <markus@markusegger.at>
2024-09-12 06:14:50 +0200

    Added autodoc extension.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,10 @@ author = 'Markus M. Egger'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
