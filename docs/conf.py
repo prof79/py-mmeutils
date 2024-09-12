a3a0c3adc76555f0164dc21b668df690d33d6471
prof79 <markus@markusegger.at>
2024-09-12 06:23:31 +0200

    Doc ref problems ...

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,15 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Paths for modules
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
