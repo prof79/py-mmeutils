6ddd403348d18d0453a26aba4413d6809407fd73
prof79 <markus@markusegger.at>
2024-09-12 16:33:28 +0200

    Doc configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -29,12 +29,17 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Paths for modules -------------------------------------------------------
-#
+# https://stackoverflow.com/questions/53668052/sphinx-cannot-find-my-python-files-says-no-module-named
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Extension options -------------------------------------------------------
-#
-myst_enable_extensions = ['colon_fence']
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+myst_enable_extensions = [
+    'colon_fence',
+    'dollarmath',
+    'fiedlist',
+    'tasklist',
+]
