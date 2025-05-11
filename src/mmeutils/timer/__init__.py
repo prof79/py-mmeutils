dc49a3ecfa6cb9bac74f787551892eb09729a426
prof79 <markus@markusegger.at>
2025-05-11 16:56:07 +0200

    Properly documented 	imer.Timer.

--- src/mmeutils/timer/__init__.py
+++ src/mmeutils/timer/__init__.py
@@ -26,23 +26,67 @@ __all__: List[str] = [
 #region Classes
 
 class TimerError(Exception):
-    """A custom exception used to report errors using the Timer class."""
+    """A custom exception type used to report errors within the Timer class.
+    """
 
 
 
 @dataclass
 class Timer(ContextDecorator):
-    """Times your code using a class, context manager, or decorator."""
+    """Times your code using a class, context manager, or decorator.
+    
+    :param name: Only required when using multiple timers in parallel.
+    :type name: str
+
+    :param text: Format string for the result message when a timer stops.
+    :type text: str
+
+    :param logger: You could set this to `print`. The callback function
+        to receive the result message when a timer stops.
+    :type logger: Callable[[str], None]
+    
+    To summarize, you can use `Timer` in three different ways:
+
+    As a class::
+
+        t = Timer(name="class")
+        t.start()
+        # Do something
+        t.stop()
+
+    As a context manager::
+
+        with Timer(name="context manager"):
+            # Do something
+
+    As a decorator::
+
+        @Timer(name="decorator")
+        def stuff():
+            # Do something
+
+    This kind of Python timer is mainly useful for monitoring the time
+    that your code spends at individual key code blocks or functions.
+    """
+
+    _start_time: Optional[float] = field(default=None, init=False, repr=False)
+    """Internal variable for the start time"""
+
+    name: Optional[str] = None
+    """Name of the timer when using multiple timers"""
+
+    logger: Optional[Callable[[str], None]] = None
+    """The callback function receiving the result message"""
+
+    text: str = "Elapsed time: {:0.4f} seconds"
+    """The format string for the result message"""
 
     timers: ClassVar[Dict[str, float]] = {}
-    name: Optional[str] = None
-    text: str = "Elapsed time: {:0.4f} seconds"
-    logger: Optional[Callable[[str], None]] = None
-    _start_time: Optional[float] = field(default=None, init=False, repr=False)
+    """A class dictionary of named timers that can run in parallel"""
 
 
     def __post_init__(self) -> None:
-        """Initialization: add timer to dict of timers"""
+        """Initialization: Adds timer to `timers` dictionary."""
         if self.name:
             self.timers.setdefault(self.name, 0)
 
@@ -50,13 +94,13 @@ class Timer(ContextDecorator):
     def start(self) -> None:
         """Starts a new timer."""
         if self._start_time is not None:
-            raise TimerError(f"Timer is running. Use .stop() to stop it.")
+            raise TimerError(f"Timer is already running. Use .stop() to stop it.")
 
         self._start_time = time.perf_counter()
 
 
     def stop(self) -> float:
-        """Stops the timer, and report the elapsed time."""
+        """Stops the timer and reports the elapsed time."""
         if self._start_time is None:
             raise TimerError(f"Timer is not running. Use .start() to start it.")
 
